1dc3f80cacd40f9f26271462b4d67376af19b662
iamsakon <sakon.bluechip@gmail.com>
2017-01-23 18:36:55 +0700

    create example (SAPM)

--- sseries-workspace/modules/sapm-web/src/main/java/com/skysoft/sseries/sapm/portlet/SapmWebmvcportletPortlet.java
+++ sseries-workspace/modules/sapm-web/src/main/java/com/skysoft/sseries/sapm/portlet/SapmWebmvcportletPortlet.java
@@ -6,18 +6,18 @@ import javax.portlet.Portlet;
 
 import org.osgi.service.component.annotations.Component;
 
-@Component(
-	immediate = true,
-	property = {
-		"com.liferay.portlet.display-category=category.sample",
-		"com.liferay.portlet.instanceable=true",
-		"javax.portlet.display-name=sapm-web Portlet",
-		"javax.portlet.init-param.template-path=/",
-		"javax.portlet.init-param.view-template=/view.jsp",
-		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=power-user,user"
-	},
-	service = Portlet.class
-)
-public class SapmWebmvcportletPortlet extends MVCPortlet {
+//@Component(
+//	immediate = true,
+//	property = {
+//		"com.liferay.portlet.display-category=category.sample",
+//		"com.liferay.portlet.instanceable=true",
+//		"javax.portlet.display-name=sapm-web Portlet",
+//		"javax.portlet.init-param.template-path=/",
+//		"javax.portlet.init-param.view-template=/view.jsp",
+//		"javax.portlet.resource-bundle=content.Language",
+//		"javax.portlet.security-role-ref=power-user,user"
+//	},
+//	service = Portlet.class
+//)
+public class SapmWebmvcportletPortlet  { //extends MVCPortlet {
 }
